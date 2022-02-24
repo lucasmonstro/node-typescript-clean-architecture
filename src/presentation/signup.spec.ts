d666d23b185380e25666473b2a3f5fecb848b60c
lucasmonstro <joao.galiano.silva@gmail.com>
2022-02-24 08:26:49 +0000

    ✅ ensure SignUpController returns errors when name/email is not provided

--- src/presentation/signup.spec.ts
+++ src/presentation/signup.spec.ts
@@ -1,9 +1,29 @@
 import SignUpController from './signup';
 
 describe('SignUpController', () => {
-  test('Should return BadRequest when no name is provided', () => {
+  test('Should return BadRequest when name is not provided', () => {
     const sut = new SignUpController();
-    const httpResponse = sut.handle();
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+
+  test('Should return BadRequest when no email is not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
   });
 });
--- src/presentation/signup.ts
+++ src/presentation/signup.ts
@@ -1,5 +1,19 @@
 export default class SignUpController {
-  handle() {
+  handle(httpRequest) {
+    const nameIsNotProvided = !httpRequest.body.name;
+    if (nameIsNotProvided) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+    const emailIsNotProvided = !httpRequest.body.email;
+    if (emailIsNotProvided) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
     return {
       statusCode: 400,
       body: {},
