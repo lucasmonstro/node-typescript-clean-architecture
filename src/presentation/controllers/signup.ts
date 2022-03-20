416081ebe79ec5b1a0c282b0a9e764265da95395
lucasmonstro <joao.galiano.silva@gmail.com>
2022-03-20 13:13:55 +0000

    :label: add SignUpPayload

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,11 +2,15 @@ import MissingParamError from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helpers';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 
+type SignUpPayload = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 export default class SignUpController {
   handle(
-    httpRequest: HttpRequest<
-      Partial<{ name: string; email: string; password: string }>
-    >,
+    httpRequest: HttpRequest<Partial<SignUpPayload>>,
   ): HttpResponse<Error> {
     const requiredFields = ['name', 'email', 'password'];
     for (const field of requiredFields) {
