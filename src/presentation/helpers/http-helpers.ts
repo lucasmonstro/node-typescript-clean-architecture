0de3f36bf46b0c4174522bbb30b2cfc9aa4cd9ec
lucasmonstro <joao.galiano.silva@gmail.com>
2022-03-20 13:54:33 +0000

    :label: add return types to http helpers

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,12 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import ServerError from '../errors/server-error';
+import { HttpResponse } from '../protocols/http';
 
-export const badRequest = (error: Error) => ({
+export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: StatusCodes.BAD_REQUEST,
   body: error,
 });
 
-export const serverError = () => ({
+export const serverError = (): HttpResponse<ServerError> => ({
   statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
   body: new ServerError(),
 });
