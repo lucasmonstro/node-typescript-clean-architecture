2f1bb83dce0456b6c5fe11f166ec3cfc9e77f8ab
lucasmonstro <joao.galiano.silva@gmail.com>
2022-03-20 18:18:57 +0000

    :white_check_mark: add sut to EmailValidatorAdapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,15 +1,17 @@
 import faker from '@faker-js/faker';
 import isEmail from 'validator/lib/isEmail';
+import { EmailValidator } from '../presentation/controllers/signup-protocols';
 import EmailValidatorAdapter from './email-validator-adapter';
 
 jest.mock('validator/lib/isEmail');
 
-describe('EmailValidatorAdapter', () => {
-  const mockedIsEmail = isEmail as jest.MockedFunction<typeof isEmail>;
+const makeSut = (): EmailValidator => new EmailValidatorAdapter();
+const mockedIsEmail = isEmail as jest.MockedFunction<typeof isEmail>;
 
+describe('EmailValidatorAdapter', () => {
   it('should return false when email is invalid', () => {
     mockedIsEmail.mockReturnValue(false);
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const invalidEmail = 'invalid_email';
     expect(sut.isValid(invalidEmail)).toBe(false);
     expect(mockedIsEmail).toHaveBeenCalledWith(invalidEmail);
@@ -17,7 +19,7 @@ describe('EmailValidatorAdapter', () => {
 
   it('should return truen when email is valid', () => {
     mockedIsEmail.mockReturnValue(true);
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const validEmail = faker.internet.email();
     expect(sut.isValid(validEmail)).toBe(true);
     expect(mockedIsEmail).toHaveBeenCalledWith(validEmail);
