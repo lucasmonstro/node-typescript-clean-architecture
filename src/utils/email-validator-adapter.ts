63f62cd59cf5cd0d3f3dc8d101be85e1f35d7980
lucasmonstro <joao.galiano.silva@gmail.com>
2022-03-20 18:29:42 +0000

    :recycle: remove DRY on EmailValidatorAdapter return type

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -2,7 +2,7 @@ import isEmail from 'validator/lib/isEmail';
 import { EmailValidator } from '../presentation/controllers/signup-protocols';
 
 export default class EmailValidatorAdapter implements EmailValidator {
-  isValid(email: string): boolean {
+  isValid(email: string): ReturnType<EmailValidator['isValid']> {
     return isEmail(email);
   }
 }
